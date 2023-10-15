fb7bb1cff81605fa0256c2f58429206e46d74693
unknown <mcjannetp@gmail.com>
2023-10-15 16:31:15 -0400

    ew

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "node-util";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+     <React.StrictMode>
+          <App />
+     </React.StrictMode>
 );
