5599389ee21f72a1a5a80f37f2e90eddcaf55abf
unknown <mcjannetp@gmail.com>
2025-01-08 16:31:15 -0500

    added polyfill

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// polyfill for promisify in the browser
+if (typeof window.promisify === "undefined") {
+	window.promisify = (fn) => {
+		return (...args) => {
+			return new Promise((resolve, reject) => {
+				fn(...args, (err, result) => {
+					if (err) reject(err);
+					else resolve(result);
+				});
+			});
+		};
+	};
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-     <React.StrictMode>
-          <App />
-     </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
